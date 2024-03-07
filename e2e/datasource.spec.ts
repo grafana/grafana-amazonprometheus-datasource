34b0c1d48fe75b808b9ab3cc2954163428df93ab
Brendan O'Handley <brendan.ohandley@grafana.com>
2024-03-07 13:34:47 -0600

    removed unnecessary test

--- e2e/datasource.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@grafana/plugin-e2e';
-
-test('should expand multi-valued variable before calling backend', async ({
-  readProvisionedDataSource,
-}) => {
-  const datasource = readProvisionedDataSource({ fileName: 'datasources.yml' });
-});

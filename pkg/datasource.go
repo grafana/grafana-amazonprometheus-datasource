6260492d0920f467e0203726e5a7966098ef7f02
ismail simsek <ismailsimsek09@gmail.com>
2024-03-13 17:19:31 +0100

    initialize promlib

---
+++ pkg/datasource.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
+	sdkhttpclient "github.com/grafana/grafana-plugin-sdk-go/backend/httpclient"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
+
+	"github.com/grafana/grafana/pkg/promlib"
+)
+
+func NewDatasource(context.Context, backend.DataSourceInstanceSettings) (instancemgmt.Instance, error) {
+	plog := backend.NewLoggerWith("logger", "tsdb.prometheus-amazon")
+	plog.Debug("Initializing")
+	return &Datasource{
+		Service: promlib.NewService(sdkhttpclient.NewProvider(), plog, extendClientOpts),
+	}, nil
+}
+
+type Datasource struct {
+	Service *promlib.Service
+}
+
+func (d *Datasource) QueryData(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
+	return d.Service.QueryData(ctx, req)
+}
+
+func (d *Datasource) CallResource(ctx context.Context, req *backend.CallResourceRequest, sender backend.CallResourceResponseSender) error {
+	return d.Service.CallResource(ctx, req, sender)
+}
+
+func (d *Datasource) GetBuildInfo(ctx context.Context, req promlib.BuildInfoRequest) (*promlib.BuildInfoResponse, error) {
+	return d.Service.GetBuildInfo(ctx, req)
+}
+
+func (d *Datasource) GetHeuristics(ctx context.Context, req promlib.HeuristicsRequest) (*promlib.Heuristics, error) {
+	return d.Service.GetHeuristics(ctx, req)
+}
+
+func (d *Datasource) CheckHealth(ctx context.Context, req *backend.CheckHealthRequest) (*backend.CheckHealthResult,
+	error) {
+	return d.Service.CheckHealth(ctx, req)
+}
+
+func extendClientOpts(ctx context.Context, settings backend.DataSourceInstanceSettings, clientOpts *sdkhttpclient.Options) error {
+	// Set SigV4 service namespace
+	if clientOpts.SigV4 != nil {
+		clientOpts.SigV4.Service = "aps"
+	}
+
+	return nil
+}

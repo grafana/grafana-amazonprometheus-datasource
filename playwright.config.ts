5429e1fb098ef579bd18ed9f04e4abc98a59e6ad
ismail simsek <ismailsimsek09@gmail.com>
2024-03-28 11:50:31 +0100

    use 10s

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Timeout for each test in milliseconds. Defaults to 30 seconds. */
-  timeout: 15000,
+  timeout: 10000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */

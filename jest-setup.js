720eaab607e20249300154aedd71ae6ae33f518f
ismail simsek <ismailsimsek09@gmail.com>
2024-06-06 17:17:07 +0200

    revert

--- jest-setup.js
+++ jest-setup.js
@@ -2,8 +2,6 @@
 import './.config/jest-setup';
 import { TextEncoder, TextDecoder } from 'util';
 
-const global = window as any;
-
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 

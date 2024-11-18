657d4c336000df1f113b3fef75361080bbf93a99
Isabella Siu <isabella.siu@grafana.com>
2024-11-18 16:17:50 -0500

    change selector?

--- e2e/configuration.spec.ts
+++ e2e/configuration.spec.ts
@@ -146,7 +146,9 @@ test.describe('Configuration tests', () => {
       name: DATA_SOURCE_NAME + 'check',
     });
 
-    await page.getByRole('switch').nth(3).setChecked(true);
+    await page
+      .getByGrafanaSelector(selectors.components.DataSource.Prometheus.configPage.incrementalQuerying)
+      .setChecked(true);
 
     expect(
       configPage.getByGrafanaSelector(selectors.components.DataSource.Prometheus.configPage.queryOverlapWindow)

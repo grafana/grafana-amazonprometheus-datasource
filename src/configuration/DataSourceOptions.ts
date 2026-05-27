07236c0e65b212719c3a3c014b3965f244b96754
Akash Pathak <akash.pathak@grafana.com>
2026-05-27 14:19:08 +0200

    fix: upgrade @grafana/prometheus to 13.1.4 for dev i18n initialization

--- src/configuration/DataSourceOptions.ts
+++ src/configuration/DataSourceOptions.ts
@@ -2,5 +2,6 @@ import { PromOptions } from '@grafana/prometheus';
 
 export interface DataSourceOptions extends PromOptions {
   'prometheus-type-migration'?: boolean;
+  sigV4Auth?: boolean;
   sigv4Service?: string;
 }
